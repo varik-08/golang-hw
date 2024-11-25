cf1242b04d9ccf7170720843b35834fbbd055fb1
Ярослав Сергеевич Волков <varik-08@mail.ru>
2024-11-25 19:51:48 +0500

    hw03_chessboard is completed (#2)
    * hw03_chessboard is completed
    
    * tidy
    
    * go.mod
    
    ---------
    
    Co-authored-by: Ярослав Волков <yaroslav.volkov@vitech.team>

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	const blackCell = '#'
+	const whiteCell = ' '
+	var size int
+
+	fmt.Print("Write size of board: ")
+
+	_, err := fmt.Scanf("%d", &size)
+	if err != nil {
+		fmt.Printf("Error read size: %v", err)
+		return
+	}
+
+	var cell rune
+	for i := 0; i < size; i++ {
+		if i%2 == 0 {
+			cell = whiteCell
+		} else {
+			cell = blackCell
+		}
+
+		for j := 0; j < size; j++ {
+			fmt.Print(string(cell))
+
+			if cell == whiteCell {
+				cell = blackCell
+			} else {
+				cell = whiteCell
+			}
+		}
+		fmt.Println()
+	}
 }
