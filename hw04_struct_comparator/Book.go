c670a09630468b0ebb5be421373f92b73843c2a0
Ярослав Сергеевич Волков <varik-08@mail.ru>
2024-11-25 23:30:37 +0500

    hw04_struct_comparator is completed (#3)
    * hw04_struct_comparator is completed
    
    * hw04_struct_comparator linter
    
    ---------
    
    Co-authored-by: Ярослав Волков <yaroslav.volkov@vitech.team>

---
+++ hw04_struct_comparator/Book.go
@@ -0,0 +1,58 @@
+package main
+
+type Book struct {
+	id     int
+	title  string
+	author string
+	year   int
+	size   int
+	rate   float32
+}
+
+func (book *Book) ID() int {
+	return book.id
+}
+
+func (book *Book) SetID() int {
+	return book.id
+}
+
+func (book *Book) Title() string {
+	return book.title
+}
+
+func (book *Book) SetTitle(title string) {
+	book.title = title
+}
+
+func (book *Book) Author() string {
+	return book.author
+}
+
+func (book *Book) SetAuthor(author string) {
+	book.author = author
+}
+
+func (book *Book) Year() int {
+	return book.year
+}
+
+func (book *Book) SetYear(year int) {
+	book.year = year
+}
+
+func (book *Book) Size() int {
+	return book.size
+}
+
+func (book *Book) SetSize(size int) {
+	book.size = size
+}
+
+func (book *Book) Rate() float32 {
+	return book.rate
+}
+
+func (book *Book) SetRate(rate float32) {
+	book.rate = rate
+}
--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -1,5 +1,49 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	book1 := Book{
+		id:     1,
+		title:  "Title 1",
+		author: "Author 1",
+		year:   2010,
+		size:   100,
+		rate:   4.5,
+	}
+	book2 := Book{
+		id:     1,
+		title:  "Title 2",
+		author: "Author 2",
+		year:   2011,
+		size:   50,
+		rate:   5,
+	}
+
+	comparator1 := Comparator{}
+	_, err := comparator1.Compare(book1, book2)
+	if err != nil {
+		fmt.Println("Comparator1: Error comparing books:", err)
+	}
+
+	comparator2 := NewComparator(Year)
+	result, err := comparator2.Compare(book1, book2)
+	if err != nil {
+		fmt.Println("Error comparing books:", err)
+	}
+	fmt.Printf("Comparator2 returned = %v\n", result)
+
+	comparator3 := NewComparator(Size)
+	result, err = comparator3.Compare(book1, book2)
+	if err != nil {
+		fmt.Println("Error comparing books:", err)
+	}
+	fmt.Printf("Comparator3 returned = %v\n", result)
+
+	comparator4 := NewComparator(Rate)
+	result, err = comparator4.Compare(book1, book2)
+	if err != nil {
+		fmt.Println("Error comparing books:", err)
+	}
+	fmt.Printf("Comparator4 returned = %v\n", result)
 }
