a39499425b7a2045e982c1343a63a140cafaf480
Ярослав Волков <yaroslav.volkov@vitech.team>
2024-11-26 11:23:12 +0500

    hw05_shapes ревью

--- hw05_shapes/circle.go
+++ hw05_shapes/circle.go
@@ -4,6 +4,14 @@ type Circle struct {
 	radius float64
 }
 
+func (c *Circle) Radius() float64 {
+	return c.radius
+}
+
+func (c *Circle) SetRadius(radius float64) {
+	c.radius = radius
+}
+
 func (c *Circle) Area() float64 {
 	return 3.14 * c.radius * c.radius
 }
--- hw05_shapes/rectangle.go
+++ hw05_shapes/rectangle.go
@@ -4,6 +4,22 @@ type Rectangle struct {
 	width, height float64
 }
 
+func (r *Rectangle) Width() float64 {
+	return r.width
+}
+
+func (r *Rectangle) SetWidth(width float64) {
+	r.width = width
+}
+
+func (r *Rectangle) Height() float64 {
+	return r.height
+}
+
+func (r *Rectangle) SetHeight(height float64) {
+	r.height = height
+}
+
 func (r *Rectangle) Area() float64 {
 	return r.width * r.height
 }
