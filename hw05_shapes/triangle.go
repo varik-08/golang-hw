1b5f20b6352b24e6c5b86527e217493089c7de12
Ярослав Сергеевич Волков <varik-08@mail.ru>
2024-11-26 11:24:08 +0500

    Hw05 shapes (#4)
    * hw05_shapes is completed
    
    * hw05_shapes tidy
    
    * hw05_shapes ревью
    
    ---------
    
    Co-authored-by: Ярослав Волков <yaroslav.volkov@vitech.team>

---
+++ hw05_shapes/triangle.go
@@ -0,0 +1,36 @@
+package main
+
+import "math"
+
+type Triangle struct {
+	a, b, c float64
+}
+
+func (t *Triangle) A() float64 {
+	return t.a
+}
+
+func (t *Triangle) SetA(a float64) {
+	t.a = a
+}
+
+func (t *Triangle) B() float64 {
+	return t.b
+}
+
+func (t *Triangle) SetB(b float64) {
+	t.b = b
+}
+
+func (t *Triangle) C() float64 {
+	return t.c
+}
+
+func (t *Triangle) SetC(c float64) {
+	t.c = c
+}
+
+func (t *Triangle) Area() float64 {
+	s := (t.a + t.b + t.c) / 2
+	return math.Sqrt(s * (s - t.a) * (s - t.b) * (s - t.c))
+}
